3eb425fcfa3fc567e8c29c93a58d5b9d6dad46c3
William Morales <114114124+UserWm@users.noreply.github.com>
2022-09-23 14:11:25 -0600

    Add files via upload

---
+++ ejercicio.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicios</title>
+</head>
+<body>
+    <form action="">
+        <label for="">Digite un nombre y evalue si inicia con M, O, P ó S</label> <br>
+        <input type="text"> <br>
+        <hr>
+        <label for="">Digite una direccion e identifique si existe la palabra casa o apartamento al incio de la cadena</label> <br>
+        <input type="text"> <br>
+        <hr>
+        <label for="">identifique al final de la cadena si el correo escrito es gmail.com</label> <br>
+        <input type="email"> <br>
+        <hr>
+        <label for="">Escriba un texto cualquiera e identifique cuantas paabras finalizan con "as"</label> <br>
+        <input type="text"> <br>
+        <hr>
+        <label for="">Identificar si el numero de telefono es de casa iniciando con 2 o celular inciando con 7</label> <br>
+        <input type="text"> <br>
+        <hr>
+        <label for="">Identificar la compañia de celular suponiendo que 79 ó 72 es tigo , 77 ó 75 es movistar y 71 ó 73 es digicel</label> <br>
+        <input type="text"> <br>
+        <hr>
+        <label for="">Identificar el patron de genero digitado en mayusculas o minusculas, masculino =1 , femenino=2</label> <br>
+        <input type="text"> <br>
+        <hr>
+        <input type="submit" value="Evaluar">
+    </form>
+</body>
+</html>
